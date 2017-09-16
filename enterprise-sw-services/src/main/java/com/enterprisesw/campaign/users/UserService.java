2aca7b2f38d4a80d8bf5099cb91b30195356ab39
Chidananda Pati <chidanandapati@Chidanandas-MacBook-Air.local>
2017-09-16 15:41:34 -0700

    2nd change

--- enterprise-sw-services/src/main/java/com/enterprisesw/campaign/users/UserService.java
+++ enterprise-sw-services/src/main/java/com/enterprisesw/campaign/users/UserService.java
@@ -13,6 +13,7 @@ public class UserService {
 	public List<User> getUsers() {
 		users.add(new User("101","User 1"));
 		users.add(new User("102","User 2"));
+		users.add(new User("103","User 3"));
 		return users;
 	}
 
