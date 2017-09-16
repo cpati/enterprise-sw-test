f7e4f0943a9748eab759e4756721fa571ecfb6bf
cpati <chidanandapati@gmail.com>
2017-09-16 15:43:34 -0700

    Update UserService.java

--- enterprise-sw-services/src/main/java/com/enterprisesw/campaign/users/UserService.java
+++ enterprise-sw-services/src/main/java/com/enterprisesw/campaign/users/UserService.java
@@ -14,6 +14,7 @@ public class UserService {
 		users.add(new User("101","User 1"));
 		users.add(new User("102","User 2"));
 		users.add(new User("103","User 3"));
+		users.add(new User("104","User 4"));		
 		return users;
 	}
 
